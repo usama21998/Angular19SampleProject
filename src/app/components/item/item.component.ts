dc88ed1467eb929594ba6b6ff3d801a1f10cb405
usama21998 <usama21998@gmail.com>
2025-02-15 17:56:13 +0400

    update with comments

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -3,11 +3,9 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { ListingService } from '../../services/listing.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Item } from '../../models/item';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { title } from 'process';
 import { NotificationService } from '../../services/toastr.service';
 @Component({
     selector: 'item',
@@ -20,41 +18,27 @@ import { NotificationService } from '../../services/toastr.service';
 export class ItemComponent {
     displayedColumns: string[] = ['title', 'description', 'actions'];
     dataSource: MatTableDataSource<any>
-    itemList = [
-        // {
-        //     title: "Item 1",
-        //     description: "Description of Item 1"
-        // },
-        // {
-        //     title: "Item 2",
-        //     description: "Description of Item 2"
-        // },
-        // {
-        //     title: "Item 3",
-        //     description: "Description of Item 3"
-        // }
-    ]
+
     selectedItemId: any = null;
     public myForm: FormGroup<any>;
 
     isEditing: boolean = false;
 
     constructor(private service: ListingService, private fb: FormBuilder, private notification: NotificationService) {
-        this.dataSource = new MatTableDataSource<any>();
-        this.dataSource.data = this.itemList;
-
-        this.myForm = this.fb.group({
+        this.dataSource = new MatTableDataSource<any>(); // data source for the table
+ 
+        this.myForm = this.fb.group({ // form group for the form
             title: ['', Validators.compose([Validators.required])],
             description: ['', Validators.compose([Validators.required])],
         });
     }
 
     ngOnInit() {
-        this.getItems();
+        this.getItems();   // get all the records
     }
 
     getItems() {
-        this.service.getAllItems().subscribe((res: any) => {
+        this.service.getAllItems().subscribe((res: any) => { // get all the records
             res = res.sort((a: any, b: any) => b.id - a.id);
             this.dataSource.data = res;
         }, (error) => {
@@ -63,7 +47,7 @@ export class ItemComponent {
     }
 
     performAction(action: string, item: any) {
-        if (action === 'delete') {
+        if (action === 'delete') { // delete the record
             this.service.deleteItem(item.id).subscribe((res: any) => {
                 this.notification.showSuccess('Item deleted successfully');
                 this.dataSource.data = this.dataSource.data.filter(a => a.id !== res.id);
@@ -71,7 +55,7 @@ export class ItemComponent {
                 this.notification.showError('Something went wrong, Please try again later');
             });
         }
-        else if (action === 'edit') {
+        else if (action === 'edit') { // edit the record
             this.isEditing = true;
             this.myForm.patchValue(item);
             this.selectedItemId = item.id;
@@ -79,10 +63,11 @@ export class ItemComponent {
     }
 
     onSubmit(isValid: boolean, value: any) {
-        if (!isValid) {
-            this.myForm.markAllAsTouched();
+        if (!isValid) {// clear the form and mark all fields as touched if the form is invalid
+            this.myForm.markAllAsTouched(); 
             return;
         }
+        // calls when updating a record
         if (this.isEditing) {
             let data = value;
             data.id = this.selectedItemId;
@@ -94,7 +79,7 @@ export class ItemComponent {
                 this.notification.showError('Something went wrong, Please try again later');
             });
         }
-        else {
+        else {// calls when adding a new record
             this.service.addItems(value).subscribe((res: any) => {
                 this.dataSource.data = [res, ...this.dataSource.data];
                 this.notification.showSuccess('Item added successfully');
@@ -106,13 +91,13 @@ export class ItemComponent {
 
     }
 
-    onClear() {
+    onClear() { // clear the form and set the isEditing flag to false
         this.myForm.reset();
         this.isEditing = false;
         this.selectedItemId = null;
     }
 
-    formTouchedDirtyRequired(form: any, formField: string) {
+    formTouchedDirtyRequired(form: any, formField: string) { // check if the form field is invalid
         if (form.get(formField).invalid && (form.get(formField).dirty || form.get(formField).touched))
             return true
         else
